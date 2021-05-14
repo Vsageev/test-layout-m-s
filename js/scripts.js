e87ae4da34eba76190de7412d92f6353e84d9ede
Vsageev <sageevlad@yandex.ru>
2021-05-14 18:19:47 +0300

    framework done

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,51 @@
     var navbarCollapse = function() {
         if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
+            $("#mainNav a").addClass("colorize");
         } else {
             $("#mainNav").removeClass("navbar-shrink");
+            $("#mainNav a").removeClass("colorize");
         }
     };
     navbarCollapse();
     $(window).on('scroll', navbarCollapse);
 
 })(jQuery);
+
+$(document).ready(function() {
+    $('.responsive').slick({
+        arrows: true,
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
