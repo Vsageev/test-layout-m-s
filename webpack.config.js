7f65c9ddededc5f414703dfaf0c4f922d8761d54
Vsageev <sageevlad@yandex.ru>
2021-05-23 20:17:24 +0300

    +- stable

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
-
-
 const path = require('path')
 
 module.exports = {
   entry: './src/app.js',
   module: {
     rules: [
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ]
+  },
+  externals:{
+    jquery: 'jQuery',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js'
-  }
+  },
 }
