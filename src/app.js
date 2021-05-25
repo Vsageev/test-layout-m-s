c855fd38797c003cd86d5fa4bffc1aa71d7f4beb
Vsageev <sageevlad@yandex.ru>
2021-05-25 12:33:25 +0300

    slight fixes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import './js/scripts.js'
 import css from "./css/styles.css";
 import slickCss from "./css/slick.css";
-// import slickThemeCss from "./css/slick-theme.css";
+import slickThemeCss from "./css/slick-theme.css";
 import bootStrapCss from "./css/bootstrap-grid.min.css";
